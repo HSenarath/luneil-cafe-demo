8e21784e9ec5b818fa24ebdc7404755eb706a806
Hiruni <hirunisenarath@gmail.com>
2023-01-06 17:34:59 -0500

    Fix missing bracket and verify that it works locally

--- app.js
+++ app.js
@@ -9,10 +9,14 @@ const app = express();
 let PORT = process.env.PORT || 5002;
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => app.listen(PORT, () => {
-    console.log('server is listening on port 3000')
+  .then(result => {
+
+    app.listen(PORT, () => {
+      console.log('server is listening on port 3000')
+    })
   })
-  .catch(err => console.log(err));
+  .catch((err)=> console.log(err))
+
 
 app.set('view engine', 'ejs');
 
