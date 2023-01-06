af712762210aeca7d5618e5469504cb919dfd1cb
Hiruni <hirunisenarath@gmail.com>
2023-01-06 17:25:47 -0500

    Update port to get it to work during deployment

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const cafeRoutes = require('./routes/cafeRoutes');
 
 const app = express();
 
-let PORT = process.env.PORT || 8000;
+let PORT = process.env.PORT || 5002;
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(result => app.listen(PORT, console.log(`Server is starting a ${PORT}`)))
+  .then(result => app.listen(PORT, () => {
+    console.log('server is listening on port 3000')
+  })
   .catch(err => console.log(err));
 
 app.set('view engine', 'ejs');
